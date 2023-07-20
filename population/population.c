83be3d33804b5cd19e9352a15cb7acf55d87d6c8
Aarnav <62804454+AarnavSin@users.noreply.github.com>
2023-07-20 16:30:54 +0530

    Write and compile code for Population Growth

---
+++ population/population.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt for start size
+    int startPopulation;
+    do {
+        startPopulation = get_int("Starting Population: ");
+    }
+    while (startPopulation < 9);
+    printf("\n");
+
+    // Prompt for end size
+    int endPopulation;
+    do {
+        endPopulation = get_int("Ending Population: ");
+    }
+    while (endPopulation < startPopulation);
+
+    // Calculate number of years until we reach threshold
+
+    int yearCounter = 0;
+
+    while(startPopulation < endPopulation) {
+        int populationBirths = startPopulation / 3;
+        int populationDeaths = startPopulation / 4;
+        startPopulation = startPopulation + populationBirths - populationDeaths; 
+        yearCounter++;
+    }
+
+    // Print number of years
+    printf("Years: %i\n", yearCounter);
+}
